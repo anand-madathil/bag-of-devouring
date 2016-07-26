69df2fb83ad221d574eb0d6b69d02e0e13b20c20
anand.k <anandkm.work@gmail.com>
2016-07-26 14:13:08 +0530

    added formalities

--- bagofdevouring.py
+++ bagofdevouring.py
@@ -8,26 +8,32 @@ choice = np.array(["yes", "no"])
 amountpulled = int(0)
 
 class dirtyworkdoer(object):
+
 	def findExpectation(self, iterations):
 		weightpulled = int(0)
 		bag = bagofdevouring(np.array([100,200,300,400,500,600,700,800,900,1000]), np.array([100,200,300,400,500,600,700,800,900,1000]))
 		for i in range (0, iterations):
 			weightpulled = weightpulled + bag.smartpull()
 		print "expectation obtained is:", (float(weightpulled) / iterations), "with", iterations, "tries"
+
 class bagofdevouring(object):
+
 	def __init__(self, values, weight):
 		self.values = values
 		self.weight = weight
 		self.weightpulled = int(0)
+
 	def pullout(self, slotno):
 		self.weightpulled = self.weightpulled + self.values[slotno]
 		self.values = np.delete(self.values, slotno)
 		for i in range(0,len(self.values - 1)):
 			self.destroy(i)
+
 	def destroy(self, slotno):
 		#destroy chance = weight of object / weight of bag + 100
 		if np.random.choice(choice, p = [float(self.values[slotno]) / (np.sum(self.values) + 100), 1 - float(self.values[slotno]) / (np.sum(self.values) + 100)]) == "yes":
 			self.values[slotno] = 0
+
 	def bagstatus(self):
 		location = str()
 		newbag = []
@@ -37,10 +43,13 @@ class bagofdevouring(object):
 			newbag = [self.values[int(i)] for i in location]
 		self.values = newbag
 		print self.values
+
 	def smartpull(self):
 		while len(self.values) > 0:
 			self.pullout(np.argmax(self.values))
 		return self.weightpulled
-comp = dirtyworkdoer()
-comp.findExpectation(input("input how many iterations you want: "))
+
+if __name__ == "__main__":
+	comp = dirtyworkdoer()
+	comp.findExpectation(input("input how many iterations you want: "))
 
