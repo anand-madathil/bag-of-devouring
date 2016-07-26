86b8a45a59b28a1b112d02e058c63cf332e4e282
anand.k <anandkm.work@gmail.com>
2016-07-26 13:58:58 +0530

    created the functions of the bag

--- bagofdevouring.py
+++ bagofdevouring.py
@@ -1,20 +1,40 @@
 import numpy as np
 
+#methods required:
+#pull out of bag
+#destroy items
+#expected yield
+choice = np.array(["yes", "no"])
+amountpulled = int(0)
+
 class bagofdevouring(object):
 	def __init__(self, values, weight):
 		self.values = values
 		self.weight = weight
-	def expectedYield(self):
-		expectedweight = float(0)
-		for i in range(0, len(self.values)):
-			if i == 0:
-				expectedweight = expectedweight + self.values[np.argmax(self.values)]
-				self.values = np.delete(self.values, np.argmax(self.values))
-			else:
-				print self.values
-				expectedweight = expectedweight + (self.values[np.argmax(self.values)] * float(self.values[np.argmax(self.values)]) / (np.sum(self.values) + 100))
-				self.values = np.delete(self.values, np.argmax(self.values))
-		print expectedweight
-bag = bagofdevouring(np.array([100,200,300]), np.array([100,200,300]))
-bag.expectedYield()
+		self.weightpulled = int(0)
+	def pullout(self, slotno):
+		self.weightpulled = self.weightpulled + self.values[slotno]
+		self.values = np.delete(self.values, slotno)
+		for i in range(0,len(self.values - 1)):
+			self.destroy(i)
+	def destroy(self, slotno):
+		#destroy chance = weight of object / weight of bag + 100
+		if np.random.choice(choice, p = [float(self.values[slotno]) / (np.sum(self.values) + 100), 1 - float(self.values[slotno]) / (np.sum(self.values) + 100)]) == "yes":
+			self.values[slotno] = 0
+	def bagstatus(self):
+		location = str()
+		newbag = []
+		for i in range (0, len(self.values - 1)):
+			if self.values[i] != 0:
+				location += str(i)
+			newbag = [self.values[int(i)] for i in location]
+		self.values = newbag
+		print self.values
+	def smartpull(self):
+		while len(self.values) > 0:
+			self.pullout(np.argmax(self.values))
+		return self.weightpulled
+bag = bagofdevouring(np.array([100,200,300,400,500,600,700,800,900,1000]), np.array([100,200,300,400,500,600,700,800,900,1000]))
+print "weight pulled from this iteration of the bag is", bag.smartpull()
+
 
