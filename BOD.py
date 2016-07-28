ec13c65096e9ace6aa18700190322aa3bf820032
anand.k <anandkm.work@gmail.com>
2016-07-28 15:49:05 +0530

    .

--- BOD.py
+++ BOD.py
@@ -5,16 +5,17 @@ def Bagofdevouring(weight, value):
 	if len(value) == 0: #base case
 		return 0
 	else:
+		
 		total = []
 		for a in range(len(value)):
-			for i in range(a) + range(a + 1, len(value)):
-				total.append(value[a] + (weight[i]/float((sum(weight) + 100))) * Bagofdevouring(np.delete(weight, i), np.delete(value,i)))
-
+				total.append(value[a] + sum([(weight[i]/float((sum(weight) + 100))) * Bagofdevouring(np.delete(weight, i), np.delete(value,i)) for i in range(a) + range(a + 1, len(value))]))
+				total.append(value[a] + (1 - sum([(weight[i]/float((sum(weight) + 100) for i in range (0, len(value))]) * Bagofdevouring(np.delete((weight, a),(value, a))))
 		return max(total)
 
 		
 #each case remove one weight, value, multiply by the chance of that item breaking. one more case exists, where nothing breaks. This chance is 1 - sum(P(i breaks) for i in weight)
 
 if __name__ == "__main__":
-	print Bagofdevouring([100,100],[100,100])
+	a =
+	print Bagofdevouring()
 
