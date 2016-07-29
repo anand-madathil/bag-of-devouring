badcfbafad6f3a8f9a1697c0f1b1c931acc186d7
anandk <anandkm.work@gmail.com>
2016-07-29 12:03:28 +0530

    test cases

---
+++ testcase.py
@@ -0,0 +1,14 @@
+import bod2
+
+if __name__ == "__main__":
+	a = [100, 200, 300]
+	b = a
+	print Bagofdevouring(a,b)
+
+	a = [300,200,100]
+	print Bagofdevouring(a,b)
+	
+	a = [100,100]
+	b = a
+	print Bagofdevouring(a,b)
+	
