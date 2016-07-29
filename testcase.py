5925bfe3c740820a8c37c7de8fd4333dc626dff8
anandk <anandkm.work@gmail.com>
2016-07-29 18:39:06 +0530

    test case

--- testcase.py
+++ testcase.py
@@ -3,12 +3,17 @@ import bod2
 if __name__ == "__main__":
 	a = [100, 200, 300]
 	b = a
-	print Bagofdevouring(a,b)
+	assert bod2.Bagofdevouring(a,b) == 462.5
 
 	a = [300,200,100]
-	print Bagofdevouring(a,b)
+	assert bod2.Bagofdevouring(a,b) == 470.8333333333333
 	
 	a = [100,100]
 	b = a
-	print Bagofdevouring(a,b)
+	assert bod2.Bagofdevouring(a,b) == 150
+
+	a = [10,100,150,250,500,750,1000,2500]
+	b = [100,200,300,400,500,1000,2000,1500]
+	print bod2.Bagofdevouring(a,b)
 	
+	#	a = [10,100,150,250,500,750,1000,2500,5000,7500,10000] b = [100,200,300,400,500,1000,2000,1500,3000,6000,4000]
