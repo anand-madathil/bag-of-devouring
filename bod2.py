7b4c54464e624131d67d8b94d2b2dc460fb7b5da
anandk <anandkm.work@gmail.com>
2016-07-29 08:13:38 +0530

    new code

--- bod2.py
+++ bod2.py
@@ -8,14 +8,14 @@ def Bagofdevouring(weight, value):
 		total = []
 	
 		for a in range(len(value)):
-                        pB =  np.array(sum([(weight[i]/float((sum(weight) + 100))) for i in range(len(value))]))
-		        pnB =  (1 - sum(weight)/(float(sum(weight) + 100)))
-		        bagDel = np.array([Bagofdevouring(np.delete(weight, [i,a]), np.delete(value,[i,a])) for i in range(len(value))])     
-		        bagnDel = Bagofdevouring(np.delete(weight, a),np.delete(value, a))	
-			total.append(value[a] + max(pB * bagDel) + pnB * bagnDel)
+                        pB =  np.array(sum([(weight[i]/float(sum(weight) + 100)) for i in range(len(value))]))
+		        pnB =  (1 - (sum(weight)/float(sum(weight) + 100)))
+		        bagDel = np.array([Bagofdevouring(np.delete(weight, [i,a]), np.delete(value, [i,a])) for i in range(len(value))])
+			bagnDel = Bagofdevouring(np.delete(weight, a),np.delete(value, a))	
+			total.append(value[a] + sum(pB * bagDel) + pnB * bagnDel)
+		print pB, bagDel
 		return max(total)
 
-		
 #each case remove one weight, value, multiply by the chance of that item breaking. one more case exists, where nothing breaks. This chance is 1 - sum(P(i breaks) for i in weight)
 
 if __name__ == "__main__":
