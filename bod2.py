ce156fe5b364c50a21b8f4a0089c69ae1023ef48
anandk <anandkm.work@gmail.com>
2016-07-29 18:33:57 +0530

    is this a memoization?

--- bod2.py
+++ bod2.py
@@ -6,20 +6,33 @@ def Bagofdevouring(weight, value):
 		return 0
 	else:
 		total = []
-	
+		bagDelcache = np.array([])
 		for a in range(len(value)):
                         wsum = sum(np.delete(weight,a))
                         pB =  np.array([(weight[i]/float(wsum+100)) for i in range(a) + range(a + 1,len(value))])
 		        pnB =  (1 - sum(pB))
-		        bagDel = np.array([Bagofdevouring(np.delete(weight, [i,a]), np.delete(value, [i,a])) for i in range(a) + range(a + 1,len(value))])
+			if a < len(bagDelcache):
+				bagDel = bagDelcache[a]
+			else:
+		        	bagDel = np.array([Bagofdevouring(np.delete(weight, [i,a]), np.delete(value, [i,a])) for i in range(a) + range(a + 1,len(value))])
+				bagDelcache = np.append(bagDelcache, bagDel)
 			bagnDel = Bagofdevouring(np.delete(weight, a),np.delete(value, a))	
 			total.append(value[a] + sum(pB * bagDel) + pnB * bagnDel)
-		print weight, np.argmax(total),total, pB, pnB
+
 		return max(total)
 
 #each case remove one weight, value, multiply by the chance of that item breaking. one more case exists, where nothing breaks. This chance is 1 - sum(P(i breaks) for i in weight)
 
 if __name__ == "__main__":
-	a = [100,200,300]
+	a = [100, 200, 300]
 	b = a
 	print Bagofdevouring(a,b)
+
+	a = [300,200,100]
+	print Bagofdevouring(a,b)
+	
+	a = [100,100]
+	b = a
+	print Bagofdevouring(a,b)
+	
+
